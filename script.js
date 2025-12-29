c35b72ea3521fb0d6ceacfc179ea656aceeeb539
Nadina Damsana <nadinadamsana11@gmail.com>
2025-12-29 19:16:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -389,6 +389,17 @@ const Blog = ({ darkMode }) => {
 const Contact = ({ darkMode }) => {
     const [showPopup, setShowPopup] = React.useState(false);
 
+    // reCAPTCHA Script එක load කිරීම (Component එක mount වූ පසු)
+    React.useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://www.google.com/recaptcha/api.js";
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+        
+        return () => document.body.removeChild(script);
+    }, []);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         
